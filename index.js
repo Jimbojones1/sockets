e36e25d305c5d57b869bec6c3446d99f6c5c4624
James Haff <jehaff@James-Haffs-MBP.attlocal.net>
2015-12-29 14:29:39 -0600

    Jimbo

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+    app = express();
+    server = require('http').createServer(app);
+    io = require('socket.io').listen(server);
+    nicknames = [];
+
+server.listen(3000);
+console.log("server is running");
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.sockets.on('connection', function(socket){
+
+  socket.on('new user', function(data, callback){
+    if(nicknames.indexOf(data) != -1){
+      callback(false);
+    }
+    else{
+      callback(true);
+      socket.nickname = data;
+      nicknames.push(socket.nickname);
+      updateNickNames()
+    }
+  })
+
+  function updateNickNames(){
+    io.sockets.emit('usernames', nicknames)
+  }
+
+  socket.on('send message', function(data){
+      io.sockets.emit('new message', data);
+  });
+
+  socket.on('disconnect', function(data){
+    if(!socket.nickname) return;
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+    updateNickNames();
+  });
+});
